1664d3c29d1906b0a83ed3eb292853e3be84e028
chrimon <43485383+chrimon@users.noreply.github.com>
2018-10-19 13:46:13 -0700

    Update libft.h

--- libft.h
+++ libft.h
@@ -16,9 +16,9 @@
 
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strchr(char *s, int c);
-int		ft_strcmp(const char *s1, const char *s2);
-char	*ft_strcpy(char *dst, const char *src);
-size_t	ft_strlen(const char *str);
+int   ft_strcmp(const char *s1, const char *s2);
+char  *ft_strcpy(char *dst, const char *src);
+size_t   ft_strlen(const char *str);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
