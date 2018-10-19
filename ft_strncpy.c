0ab88dacc537577c9d3c5eab761360ccb6144903
chrimon <43485383+chrimon@users.noreply.github.com>
2018-10-19 13:31:03 -0700

    Update ft_strncpy.c

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -17,10 +17,14 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 	size_t i;
 
 	i = 0;
-	while(src[i++] != '\0' && i < len)
+	while(src[i] != '\0' && i < len)
+	{	
 		dst[i] = src[i];
-	while (i++ < len)
+		i++;
+	}	
+	while (i < len)
+	{	
 		dst[i] = '\0';
-	dst[i] = '\0';
+	}	
 	return(dst);
 }
