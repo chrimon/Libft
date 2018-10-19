71169c5e7ad5af4127dd92cc58c6473fba1e8802
chrimon <43485383+chrimon@users.noreply.github.com>
2018-10-19 14:11:44 -0700

    Update ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -15,8 +15,11 @@ char	*ft_strcpy(char *dst, const char *src)
 	int i;
 
 	i = 0;
-	while(src[i++] != '\0')
+	while(src[i] != '\0')
+	{	
 		dst[i] = src[i];
+		i++;
+	}	
 	dst[i] = '\0';
 	return(dst);
 }
