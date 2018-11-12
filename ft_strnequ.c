a779765db74e3a64b368dc095e10999db72a9770
Chris Mondev <chmondev@e1z2r5p9.42.us.org>
2018-11-12 15:06:29 -0800

    rog

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: chmondev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 15:03:23 by chmondev          #+#    #+#             */
-/*   Updated: 2018/11/12 11:12:26 by chmondev         ###   ########.fr       */
+/*   Updated: 2018/11/12 15:06:14 by chmondev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t		i;
 
 	i = 0;
-	if (!s1 || !s2 || (ft_strlen(s1) != ft_strlen(s2)))
+	if (!s1 || !s2 || (ft_strlen(s1) != ft_strlen(s2)) || !n)
 		return (0);
 	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
